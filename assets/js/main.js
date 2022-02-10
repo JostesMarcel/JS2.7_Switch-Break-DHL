2d433dedffdbe491c2065901c4d067ff83eec6ff
JostesMarcel <jostes77@googlemail.com>
2022-02-10 15:11:48 +0100

    JS2.7_Switch-Break-DHL

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+let mylist = document.getElementById("mylist")
+let out = document.getElementById("masse")
+
+function showtxt() {
+    switch (true) {
+        case mylist.value == 0:
+            out.innerHTML = `<b>Brief und Postkarte</b> <br>
+            L: 10 - 23,5 cm
+            B: 7 - 12,5 cm
+            H: bis 1 cm`
+            break;
+        
+        case mylist.value == 1:
+            out.innerHTML = `<b>DHL Paket 2 kg</b> <br>
+            bis 60 x 30 x 15 cm`
+            break;
+
+        case mylist.value == 2:
+            out.innerHTML = `<b>DHL Paket 5 kg</b> <br>
+            bis 120 x 60 x 60 cm`
+            break;
+
+        case mylist.value == 3:
+            out.innerHTML = `<b>DHL Paket 10 kg</b> <br>
+            bis 120 x 60 x 60 cm`
+            break;
+    
+        default:
+            out.innerHTML = `<b>Extra große Größe</b>`
+            break;
+    }
+}
